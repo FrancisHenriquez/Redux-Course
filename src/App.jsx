b72f45ed0c78c541ebd1ff04cb16e081eb532f5d
FrancisHenriquez <francishenriquez07@gmail.com>
2022-11-07 10:43:56 -0400

    incraceby decrace

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import { useDispatch, useSelector } from 'react-redux'
-import { increment } from './store/slices/counter/counterSlice'
+import { decrement, increment, incrementBy } from './store/slices/counter/counterSlice'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,16 +14,19 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={ () => dispatch ( increment() ) }>
-          count is { counter }
-        </button>
-  
+        <p> count is: { counter }</p>
+        <p>
+          <button type="button" onClick={ () => dispatch( increment() )}>
+            Increment 
+          </button> 
+          <button type="button" onClick={ () => dispatch ( decrement() )}>
+            Decrement
+          </button>
+          <button type="button" onClick={ () => dispatch (incrementBy(2) )}>
+            Increment by 2 
+          </button>
+        </p>
+       
       </div>
      
     </div>
