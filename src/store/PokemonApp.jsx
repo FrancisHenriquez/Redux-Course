571e6960b265e2cfb102e87695bfed92e568dabc
FrancisHenriquez <francishenriquez07@gmail.com>
2022-11-08 09:35:45 -0400

    axios

--- src/store/PokemonApp.jsx
+++ src/store/PokemonApp.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { getPokemons } from './pokemon/thunks'
 
 export const PokemonApp = () => {
+  const dispatch = useDispatch()
+
+  useEffect( () => {
+    dispatch( getPokemons())
+  })
+
+useEffect(() => {
+
+
+  return () => {
+   
+  }
+}, [])
+
+
+
+
   return (
    <>
    <h1>PokemonApp</h1>
    <hr />
-
    <ul>
         <li>Hola</li>
         <li>Hola</li>
